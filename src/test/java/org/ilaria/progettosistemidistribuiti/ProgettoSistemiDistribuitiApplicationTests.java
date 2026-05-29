13b2cdacb9ad64b8bda0b469c5bcbd28cadfc8eb
ilaria <ilarialan12@gmail.com>
2026-05-29 20:12:39 +0200

    create project and add dependency

---
+++ src/test/java/org/ilaria/progettosistemidistribuiti/ProgettoSistemiDistribuitiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilaria.progettosistemidistribuiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSistemiDistribuitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
